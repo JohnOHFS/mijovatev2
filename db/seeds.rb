0b50e860d4b202ae810f0a9f5e603e67a669e156
John Viercinski <Johnnavy1351@yahoo.com>
2015-10-23 19:07:57 -0400

    removed seeds.rb text

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-granitecolors = [
- {url: "/assets/granite/absolute_black.jpg", name: "Absolute Black"},
- {url: "/assets/granite/african_ivory.jpg", name: "African Ivory"},
- {url: "/assets/granite/african_rainbow.jpg", name: "African Rainbow"}
- # this has to be manual unless you pull it from a CSV or something
-]
-
-granitecolors.each do |gc|
-   Granitecolor.find_or_create_by(url: gc.url, name: gc.name)
-end
