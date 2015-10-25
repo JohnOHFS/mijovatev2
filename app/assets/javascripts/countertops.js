b8c2caa464a6f699e02005fcc451bd9282cc1c0a
John Viercinski <Johnnavy1351@yahoo.com>
2015-10-24 22:10:51 -0400

    Added jQuery functionality to Countertop form

--- app/assets/javascripts/countertops.js
+++ app/assets/javascripts/countertops.js
@@ -10,3 +10,26 @@ $("select:not(#countertop_countertype_id)").val('0').imagepicker({
 	
  });    
 
+$(document).ready(function() {
+
+    $('#granitecolor').hide();
+    $('#marblecolor').hide();
+
+    $('#countertype').change(function () {
+        if ($('#countertype option:selected').text() == "Granite"){
+            $('#granitecolor').show();
+            $('#marblecolor').hide();
+
+        }
+        else if ($('#countertype option:selected').text() == "Marble"){
+            $('#granitecolor').hide();
+            $('#marblecolor').show();
+
+        }
+        else if ($('#countertype option:selected').text() == "Soapstone"){
+            $('#granitecolor').hide();
+            $('#marblecolor').hide();
+
+        }
+    }); 
+});
